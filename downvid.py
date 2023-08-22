179ab546d48c17ab26672158300d4685ee07dfa9
Blaing7542 <127663348+Blaing7542@users.noreply.github.com>
2023-08-22 10:44:43 +0300

    Version: 1.0

---
+++ downvid.py
@@ -0,0 +1,13 @@
+# Создано Blaing.
+from pytube import YouTube
+
+print("Отправьте ссылку на видео.")
+video_url = input()
+
+try:
+    yt = YouTube(video_url)
+    video = yt.streams.get_highest_resolution()
+    video.download()
+    print("Ваше видео установлено! Оно находится в той же папке, где и код.")
+except Exception as e:
+    print("Произошла ошибка при установке видео:", str(e))
