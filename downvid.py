9ca037a2fff396b3c9bb544e095776fffcc46df7
Blaing <127663348+Blaing7542@users.noreply.github.com>
2024-12-28 16:33:19 +0300

    update 1.2

--- downvid.py
+++ downvid.py
@@ -1,14 +1,37 @@
-from pytube import YouTube
-import time
+# pip install yt_dlp
+import os
+import yt_dlp
 
-video_url = input("link: ")
 
-try:
-    yt = YouTube(video_url)
-    video = yt.streams.get_highest_resolution()
-    video.download()
-    print("downloaded!")
-except Exception as e:
-    print(str(e))
-    time.sleep(3)
-    exit()
+def download_video():
+    url = input("link: ")
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': '%(title)s.%(ext)s',
+        'noplaylist': True,
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }],
+        'quiet': False,
+        'verbose': True
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            video_title = info_dict.get('title', 'unknown')
+            video_duration = info_dict.get('duration', 0)
+            video_file = f"{video_title}.mp4"
+            video_size = os.path.getsize(video_file)
+            duration_str = f"{video_duration // 60} min {video_duration % 60} sec"
+            size_str = f"{video_size / (1024 * 1024):.2f} МБ"
+            print(f"downloaded!\n"
+                  f"name: {video_title}\n"
+                  f"duration: {duration_str}\n"
+                  f"size: {size_str}\n"
+                  f"file name: {video_file}")
+    except Exception as e:
+        print(f"{str(e)}")
+
+if __name__ == "__main__":
+    download_video()
